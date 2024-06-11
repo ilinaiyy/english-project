439a35b83416e72ad103629ac1447e4795d60660
ilinaiyy <139927342+ilinaiyy@users.noreply.github.com>
2024-06-10 22:13:40 -0700

    Create neighborhood.java

---
+++ neighborhood.java
@@ -0,0 +1,17 @@
+public class Neighborhood {
+	private String color; 
+	//oldham = red
+	//skystead = green
+	
+	public Neighborhood(String color) {
+		this.color = color; 
+	}
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getColor() {
+		return color; 
+	}
+}
+
